e5fdf0cccd9c2b3df533cd740b2e359c8d94cf9e
bagineer <bih198721@gmail.com>
2021-10-23 19:42:00 +0000

    smp baseline version 1.6

--- smp/v1/smp_train.py
+++ smp/v1/smp_train.py
@@ -12,6 +12,7 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from utils import label_accuracy_score, add_hist
 from custom import CustomDataset, collate_fn
+from matplotlib import pyplot as plt
 
 def parse_args():
     """
@@ -27,7 +28,7 @@ def parse_args():
             deterministic : Set seed for reproducibility
                 (default : False)
             
-            private : Log result in private wandb entity
+            private : Log result to private wandb entity
                 (default : False)
     """
     parser = argparse.ArgumentParser(description='Train Segmentation Model')
@@ -41,7 +42,7 @@ def parse_args():
 
     parser.add_argument('--private',
         action='store_true',
-        help='whether to log in private wandb entity')
+        help='whether to log to private wandb entity')
     
     args = parser.parse_args()
 
@@ -191,7 +192,18 @@ def save_model(model, saved_dir, file_name, debug=False):
     output_path = os.path.join(saved_dir, file_name)
     torch.save(check_point, output_path)
 
-def train(num_epochs, model, model_name, train_loader, val_loader, criterion, optimizer, saved_dir, val_every, device, debug):
+def log_lr(lr_list):
+    """
+    Log learning rate plot to wandb.
+
+    Args:
+        lr_list (list) : List of learning rates and steps in unit of epoch
+            e.g. [[lr_1, setp_1], [lr_2, setp_2], ..., [lr_last, setp_last]]
+    """
+    table = wandb.Table(data=lr_list, columns=['Epoch', 'Learning rate'])
+    wandb.log({'test/lr' : wandb.plot.line(table, 'Epoch', 'Learning rate')})
+
+def train(num_epochs, model, model_name, train_loader, val_loader, criterion, optimizer, scheduler, saved_dir, val_every, device, debug):
     """
     Train segmentation model.
 
@@ -219,6 +231,8 @@ def train(num_epochs, model, model_name, train_loader, val_loader, criterion, op
     best_loss = 9999999
     best_mIoU = -1
     best_epoch = 0
+    steps = 0
+    lr_list = []
     cats = ['Backgroud',
             'General trash',
             'Paper',
@@ -260,6 +274,7 @@ def train(num_epochs, model, model_name, train_loader, val_loader, criterion, op
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            scheduler.step()
             
             outputs = torch.argmax(outputs, dim=1).detach().cpu().numpy()
             masks = masks.detach().cpu().numpy()
@@ -274,12 +289,14 @@ def train(num_epochs, model, model_name, train_loader, val_loader, criterion, op
             mean_loss += loss.item()
             mean_fwavacc += fwavacc
             mean_IoU += np.array(IoU)
+            steps += 1
+            lr_list.append([steps/len(train_loader), scheduler.get_last_lr()[0]])
             
             # step 주기에 따른 loss 출력
             if (step + 1) % 25 == 0:
                 print(f'Epoch [{epoch+1}/{num_epochs}], Step [{step+1}/{len(train_loader)}], \
                         Loss: {round(loss.item(),4)}, mIoU: {round(mIoU,4)}')
-
+        
         # calculate metric
         mean_acc /= len(train_loader)
         mean_acc_each_cls /= len(train_loader)
@@ -315,6 +332,7 @@ def train(num_epochs, model, model_name, train_loader, val_loader, criterion, op
                 if (epoch + 1) % save_interval == 0:
                     save_model(model, saved_dir, file_name=f'{model_name}_{epoch+1}.pt', debug=debug)
         wandb.log({'epoch/best_epoch':best_epoch}, step=(epoch+1))
+    log_lr(lr_list)
     save_model(model, saved_dir, file_name=f'{model_name}_last.pt', debug=debug)
 
 def validation(epoch, model, data_loader, criterion, device):
@@ -426,7 +444,7 @@ if __name__ == '__main__':
 
     # Define model
     ## TODO
-    encoder_name = 'tu-xception65'
+    encoder_name = 'tu-xception41'
 
     model = smp.DeepLabV3Plus(
         encoder_name=encoder_name,
@@ -439,7 +457,7 @@ if __name__ == '__main__':
     ## TODO
     val_every = 1
     batch_size = 8   # Mini-batch size
-    num_epochs = 50
+    num_epochs = 30
     learning_rate = 0.0001
     saved_dir = os.path.join('/opt/ml/segmentation/saved', model_name)
     saved_dir = make_save_dir(saved_dir, debug)
@@ -458,7 +476,7 @@ if __name__ == '__main__':
                                 shuffle=True,
                                 num_workers=4,
                                 collate_fn=collate_fn,
-                                ratio=0.2,
+                                ratio=0.1,
                                 debug=debug)
 
     val_loader = make_dataloader(mode='val',
@@ -466,7 +484,7 @@ if __name__ == '__main__':
                                 shuffle=False,
                                 num_workers=4,
                                 collate_fn=collate_fn,
-                                ratio=0.2,
+                                ratio=0.1,
                                 debug=debug)
 
     # Loss function 정의
@@ -475,7 +493,12 @@ if __name__ == '__main__':
 
     # Optimizer 정의
     ## TODO
-    optimizer = torch.optim.Adam(params = model.parameters(), lr = learning_rate, weight_decay=1e-6)
+    optimizer = torch.optim.Adam(params = model.parameters(), lr = learning_rate, weight_decay=1e-5)
+
+    # Scheduler 정의
+    ## TODO
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(train_loader)*5)
+    # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=len(train_loader)*5, gamma=0.8)
 
     wandb_config = {
         'val_every': val_every,
@@ -495,4 +518,4 @@ if __name__ == '__main__':
     wandb.run.save()
 
     # Start training
-    train(num_epochs, model, model_name, train_loader, val_loader, criterion, optimizer, saved_dir, val_every, device, debug)
+    train(num_epochs, model, model_name, train_loader, val_loader, criterion, optimizer, scheduler, saved_dir, val_every, device, debug)
